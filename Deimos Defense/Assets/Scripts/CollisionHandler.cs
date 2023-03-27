4ef2036bc2ef6213e496230d6d0ed4ff8801cc0a
kevinblucodes <125173969+kevinblucodes@users.noreply.github.com>
2023-03-27 09:16:17 -0400

    Added Crash Sequnce

--- Deimos Defense/Assets/Scripts/CollisionHandler.cs
+++ Deimos Defense/Assets/Scripts/CollisionHandler.cs
@@ -1,15 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 1f;
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log(this.name + "--Collided with" + other.gameObject.name);
     }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{this.name} **Triggered by** {other.gameObject.name}");
+        //Debug.Log($"{this.name} **Triggered by** {other.gameObject.name}");
+        StartCrashSequence();
+    }
+
+    private void StartCrashSequence()
+    {
+        GetComponent<PlayerControls>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
