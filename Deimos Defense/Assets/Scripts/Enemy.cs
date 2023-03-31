e7f48090b96d42bfc22b8c2925bcd5c99c4e4d97
kevinblucodes <125173969+kevinblucodes@users.noreply.github.com>
2023-03-30 21:18:45 -0400

    Added Score Board and Hit VFX

--- Deimos Defense/Assets/Scripts/Enemy.cs
+++ Deimos Defense/Assets/Scripts/Enemy.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 1;
 
-   void OnParticleCollision(GameObject other)
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindAnyObjectByType<ScoreBoard>();    
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitPoints < 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        hitPoints--;
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
-
-        //Debug.Log("Hit by " + other.gameObject);
     }
+
 }
--- Deimos Defense/Assets/Scripts/ScoreBoard.cs
+++ Deimos Defense/Assets/Scripts/ScoreBoard.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score;
+    TMP_Text scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "00";
+    }
 
     public void IncreaseScore(int amountToIncrease)
     {
-        score = score + amountToIncrease;
+        score += amountToIncrease;
+        Debug.Log($"Score is now: {score}");
+        scoreText.text = score.ToString();
     }
 }
